e48bf12fb1f092b47f2931b1daeb40486102112c
EfarovEmil <efarov-emil@mail.ru>
2021-10-19 17:14:28 +0300

    register-form

---
+++ fast-company/src/app/components/ui/registerForm.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import { validator } from "../../utils/validator";
+import api from "../../api";
+import TextField from "../common/form/textField";
+import SelectField from "../common/form/selectField";
+
+const RegisterForm = () => {
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        profession: ""
+    });
+    const [professions, setProfession] = useState();
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => setProfession(data));
+    }, []);
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    const validatorConfog = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Email введен некорректно"
+            }
+        },
+        password: {
+            isRequired: {
+                message: "Пароль обязательна для заполнения"
+            },
+            isCapitalSymbol: {
+                message: "Пароль должен содержать хотя бы одну заглавную букву"
+            },
+            isContainDigit: {
+                message: "Пароль должен содержать хотя бы одно число"
+            },
+            min: {
+                message: "Пароль должен состаять миниму из 8 символов",
+                value: 8
+            }
+        },
+        profession: {
+            isRequired: { message: "Обязательно выберите вашу профессию" }
+        }
+    };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfog);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const isValid = Object.keys(errors).length === 0;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Электронная почта"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+                error={errors.email}
+            />
+            <TextField
+                label="Пароль"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                error={errors.password}
+            />
+            <SelectField
+                label="Выберите вашу профессию"
+                defaultOption="Choose.."
+                onChange={handleChange}
+                options={professions}
+                value={data.profession}
+                error={errors.profession}
+            />
+            <button
+                type="submit"
+                disabled={!isValid}
+                className="btn btn-primary w-100 mx-auto"
+            >
+                Submit
+            </button>
+        </form>
+    );
+};
+
+export default RegisterForm;
--- fast-company/src/index.js
+++ fast-company/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import App from "./app";
+import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
