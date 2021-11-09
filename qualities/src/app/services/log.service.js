1aef6537c267c48e486f05dbb58fde1807b88c9f
EfarovEmil <efarov-emil@mail.ru>
2021-11-09 06:59:53 +0300

    qualitites task and server

---
+++ qualities/src/app/services/log.service.js
@@ -0,0 +1,25 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://c26d9fed7fd3459ea315a7ce6c4991f2@o1064367.ingest.sentry.io/6055192",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+const logger = {
+  init,
+  log,
+};
+
+export default logger;
